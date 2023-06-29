9d1dbb1f3e6258c42eb9fffba9f1751a5fc8fd58
rachancheet <raxxsngh37@gmail.com>
2023-06-29 13:27:19 +0530

    auto commit

--- adventofcode.cpp
+++ adventofcode.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 // ehefsiseufefuasihfiehsaoeshfiseiseuifiueahshfosehfishfhdihfw
 // finding the index after 14 consecutive unique characters
-int (int arr[],int size){
-    bitset<32> uni 
+int q1(string s){
+    int a =0;
+    for(int i=0; i<s.length(); i++){
+        s[i]%32;
+    }    
 
 };
 int main(){
 
- 
+cout<<(int)'y';
+
+
 }
 
