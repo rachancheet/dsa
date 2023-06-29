f6ae7565d3355545b1f13f4471f90457a0415a7b
rachancheet <raxxsngh37@gmail.com>
2023-06-29 13:42:21 +0530

    auto commit

--- adventofcode.cpp
+++ adventofcode.cpp
@@ -6,9 +6,22 @@ using namespace std;
 // ehefsiseufefuasihfiehsaoeshfiseiseuifiueahshfosehfishfhdihfw
 // finding the index after 14 consecutive unique characters
 int q1(string s){
-    int a =0;
+    // int a =0;
+    int arr = (int *) calloc(26*4);
     for(int i=0; i<s.length(); i++){
-        s[i]%32;
+        for(int j   = i; j  <  i+14;j++){
+            // 1<<(s[i]%32);
+            if(arr[((int)s[j]%32)]){
+                i = j+1;
+                break;
+                
+            }
+            else{
+                arr[((int)s[j]%32)] =1;
+            }
+
+        }
+
     }    
 
 };
